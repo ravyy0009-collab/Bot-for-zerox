38eddac88485a17850606b629c7fc178ae5f94ef
ravyy0009-collab <ravyy.0009@gmail.com>
2026-02-11 20:17:04 -0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ from telegram.ext import (
 )
 
 # ================== CONFIG ==================
-BOT_TOKEN = os.getenv("8527242790:AAEjwurN9fG3nGK9IfTtgwFd7AKHNyAyGjY")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 SUPPORT_GROUP_ID = -1003809204938  # Your support group ID
 # ============================================
 
